96dc4ed32cde466d2a06afe3fdd518d1fce4ee92
antrimo <kartikeyai246@gmail.com>
2025-07-10 11:42:16 +0530

    added tabs

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -44,110 +44,155 @@ class _DetailScreenState extends State<DetailScreen> {
 
             final data = snapshot.data!;
 
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey.shade300,
-                            width: 1,
-                          ),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(Icons.arrow_back),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0,
-                      vertical: 12,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
+            return DefaultTabController(
+              length: 2,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          height: 50,
+                          width: 50,
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
                             border: Border.all(
                               color: Colors.grey.shade300,
                               width: 1,
                             ),
-                            image: DecorationImage(
-                              image: NetworkImage(data['logo']),
-                              fit: BoxFit.cover,
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(Icons.arrow_back),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 12,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
+                                color: Colors.grey.shade300,
+                                width: 1,
+                              ),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                ),
+                              ],
+                            ),
+                            child: Image.network(
+                              data['logo'],
+                              height: 48,
+                              width: 48,
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          data['company_name'],
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            height: 1.5,
-                            letterSpacing: -0.01 * 16,
+                          const SizedBox(height: 12),
+                          Text(
+                            data['company_name'],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              height: 1.5,
+                              letterSpacing: -0.01 * 16,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(
-                          data['description'],
-                          style: const TextStyle(fontSize: 12, height: 1.5),
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8,
-                                vertical: 4,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.blue.shade100,
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              child: Text(
-                                "ISIN: ${data['isin']}",
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue,
+                          const SizedBox(height: 12),
+                          Text(
+                            data['description'],
+                            style: const TextStyle(fontSize: 12, height: 1.5),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 4,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue.shade100,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Text(
+                                  "ISIN: ${data['isin']}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(width: 12),
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8,
-                                vertical: 4,
+                              const SizedBox(width: 12),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 4,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.green.shade100,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Text(
+                                  data['status'],
+                                  style: const TextStyle(color: Colors.green),
+                                ),
                               ),
-                              decoration: BoxDecoration(
-                                color: Colors.green.shade100,
-                                borderRadius: BorderRadius.circular(5),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+
+                          const TabBar(
+                            isScrollable: true,
+                            labelColor: Colors.black,
+                            indicatorColor: Colors.black,
+                            tabAlignment: TabAlignment.start,
+                            labelPadding: EdgeInsets.only(right: 20),
+                            tabs: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Tab(child: Text('Tab 1')),
                               ),
-                              child: Text(
-                                data['status'],
-                                style: const TextStyle(color: Colors.green),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Tab(child: Text('Tab 2')),
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                      ],
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+
+                    SizedBox(
+                      height: 200,
+                      child: const TabBarView(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: Text("Tab 1 Content"),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: Text("Tab 2 Content"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tap/detail_screen.dart';
 import 'package:tap/home.dart';
 
 void main() {
